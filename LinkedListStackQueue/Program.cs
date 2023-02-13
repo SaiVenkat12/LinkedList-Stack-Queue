2ed5ad99ef00b964a47f53cf82f7350055f28521
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-13 22:37:28 +0530

    [Sai Venkat] Added elements to the LinkedList Queue

--- LinkedListStackQueue/Program.cs
+++ LinkedListStackQueue/Program.cs
@@ -12,13 +12,36 @@ namespace LinkedListStackQueue
         {
             Console.WriteLine(" Linked List Stack and Queue");
             Stack stack= new Stack();
-            stack.Add(50);
-            stack.Add(30);
-            stack.Add(70);
-            stack.Display();
-            stack.isEmpty();
-            stack.Display();
-            Console.ReadLine();
+            Queue queue = new Queue();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Please Choose \n1.Stack \n2.Queue \n3.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        stack.Add(56);
+                        stack.Add(30);
+                        stack.Add(70);
+                        stack.Display();
+                        stack.isEmpty();
+                        stack.Display();
+                        break;
+                    case 2:
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        queue.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the Correct Number !");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ LinkedListStackQueue/Queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListStackQueue
+{
+    internal class Queue
+    {
+        public Node head;
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            { 
+                head = newNode;
+                Console.WriteLine("{0} is inserted into the Queue ", head.data);
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+                Console.WriteLine("{0} is inserted into the Queue ", newNode.data);
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("QUEUE");
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
